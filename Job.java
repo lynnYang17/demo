ce51c22f4d2e5e4ff28a1e2a2f2586f6f90ed96c
yanglin3 <yanglin3@xiaomi.com>
2019-05-26 20:16:15 +0800

    [yanglin]: add new method
    1. bubble sort
    2. move the dumplication to the end

--- Job.java
+++ Job.java
@@ -19,6 +19,24 @@ class Job{//To find a new job, Fighting!
 		System.out.println(input3);
 		float ret3 = mJob.calculate(input3);
 		System.out.println("calculate result: " + input3 + " = " + ret3);
+
+		/* ---------- 4. bubble sort -----------*/
+		int[] input4 = {1, 9, 3, 6, 7, 8, 0, 12};
+		//System.out.println("input4" + input4.toString());//dont use
+		mJob.bubbleSort(input4);
+		//System.out.println("input4 after sort" + input4.toString()); //dont use
+		for(int i = 0; i < input4.length; i++){
+			System.out.print(input4[i] + " ");
+		}
+		System.out.println();
+		/*--------- move the dumpliaction to the end ---------*/
+		int[] input5 = {1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6};
+		System.out.print("move the dumplication to the end: ");
+		int[] ret5 = mJob.moveTheDuplication(input5);
+		for(int i = 0; i < input5.length; i++){
+			System.out.print(input5[i] + " ");
+		}
+		System.out.println();
 	}
 	//1. find the first target in a int[] by Binary_Search
 	public int binarySearch(int[] input, int target){//2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 10
@@ -142,5 +160,53 @@ class Job{//To find a new job, Fighting!
 		}
 		return ret;
 	}
-	//
+	// Bubble sort
+	public void bubbleSort(int[] input){
+		//从小到大排列，每轮结束后，最大的沉到最下边或者最小的浮到最上边
+		for(int i = 0; i < input.length; i ++){
+			for (int j = 0; j < input.length - 1 - i; j++){
+				if(input[j] > input[j + 1]){
+					int temp = input[j];
+					input[j] = input[j + 1];
+					input[j + 1] = temp;
+				}
+			}
+		}
+	}
+	// Move duplication to the end
+	public int[] moveTheDuplication(int[] input){
+		//input: 1 1 2 2 3 3 3 4 4 5 5 6
+		// i = 0, j = 0: 1 1 2 2 3 3 3 4 4 5 5 6
+		// i = 0, j = 1: 1 1 2 2 3 3 3 4 4 5 5 6
+		// i = 0, j = 2: 1 2 1 2 3 3 3 4 4 5 5 6
+		// i = 1, j = 3: 1 2 1 2 3 3 3 4 4 5 5 6
+		// i = 1, j = 4: 1 2 3 2 1 3 3 4 4 5 5 6
+		// i = 2, j = 5: 1 2 3 2 1 3 3 4 4 5 5 6
+		// i = 2, j = 6: 1 2 3 2 1 3 3 4 4 5 5 6
+		// i = 2, j = 7: 1 2 3 4 1 3 3 2 4 5 5 6
+		// i = 3, j = 8: 1 2 3 4 1 3 3 2 4 5 5 6
+		// i = 3, j = 9: 1 2 3 4 5 3 3 2 4 1 5 6
+		// i = 4, j = 10: 1 2 3 4 5 3 3 2 4 1 5 6
+		// i = 4, j = 10: 1 2 3 4 5 6 3 2 4 1 5 3
+		// i = 5, j = 11: 不进入j循环
+		//目的是找到第一次出现的数，并且放到它该在的位置
+		//1. 当遇到了和当前i位置不相同的数，就把它放到i位置，i++
+		//2. 用j来控制遍历
+		for(int i = 0; i < input.length; i++){
+			int j = 0;
+			for(j = 0; j < input.length; j++){
+				if(input[j] > input[i]){
+					int temp = input[i + 1];
+					input[i + 1] = input[j];
+					input[j] = temp;
+					i++;
+				};
+			}
+			if(j == input.length){
+				break;
+			}
+		}
+		return input;
+	}
+
 }
