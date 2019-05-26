d572fc612045e703cedacfa76e5057d8e6ac8432
yanglin3 <yanglin3@xiaomi.com>
2019-05-26 21:17:16 +0800

    [yanglin]: learn more about java equals and "=="

--- Learn.java
+++ Learn.java
@@ -6,13 +6,21 @@ class Learn {
 		//Stack
 		Stack<Integer> st = new Stack<Integer>();
 		System.out.println("isStackEmpty: " + (st.empty() ? "empty" : st.size()));
-		//Stack<Integer> st = {1, 2, 3, 4, 5, 6};
+		//Stack<Integer> st = {1, 2, 3, 4, 5, 6}; //error
 		mLearn.showPush(st, 1209);
 		System.out.println("isStackEmpty: " + (st.empty() ? "empty" : st.size()));
 		mLearn.showPush(st, 100);
 		mLearn.showPush(st, 1000);
 		mLearn.showPush(st, 10000);
 		mLearn.showPop(st);
+
+		String str1 = "hello";
+		String str2 = "hello";
+		System.out.println("str1 == str2: " + (str1 == str2));
+		String str3 = new String("hello");
+		System.out.println("str3 == str1: " + (str3 == str1));
+		String str4 = new String("hello");
+		System.out.println("str3 == str4: " + (str3 == str4));
 	}
 	private void showPush(Stack<Integer> st, int a){
 		//System.out.println(TAG +  ": showPush start");
